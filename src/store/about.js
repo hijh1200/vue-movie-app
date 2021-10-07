0ec3dee068107bd3eefdc679794f8ecc0066c527
hijh1200 <hijh1200@naver.com>
2021-10-07 20:09:26 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'PJH',
+    email: 'hijh1200@naver.com',
+    blog: 'https://github.com/hijh1200',
+    phone: '+82 010-4016-8289',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
